b5a683b63abf569ed75f28e2c518a9d7b6c3287f
Mateusz Siekierka <mat.siekierka96@gmail.com>
2020-10-20 11:56:36 +0200

    refactored map impl to abstract ID generation. closes #19.

--- pet-clinic-web/src/main/java/example/spring/project/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/example/spring/project/springpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Adam");
         owner1.setLastName("Kowal");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Julia");
         owner2.setLastName("Nowak");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Wojciech");
         vet1.setLastName("Niedziela");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Kacper");
         vet2.setLastName("Wujek");
 
