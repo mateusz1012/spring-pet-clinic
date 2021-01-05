cb704d9446e2d64869cb779e3b06bcfda2988ee3
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-01-05 14:02:00 +0100

    added specialities, added to vet. closes #23.

---
+++ pet-clinic-data/src/main/java/example/spring/project/springpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package example.spring.project.springpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
