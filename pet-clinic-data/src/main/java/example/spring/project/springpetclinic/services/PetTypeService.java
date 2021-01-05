bf65ae6d7ee45bcfc33741c96c2373ebbc2fff90
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-01-05 14:43:07 +0100

    added PetTypeService. closes #25.

---
+++ pet-clinic-data/src/main/java/example/spring/project/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package example.spring.project.springpetclinic.services;
+
+import example.spring.project.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
