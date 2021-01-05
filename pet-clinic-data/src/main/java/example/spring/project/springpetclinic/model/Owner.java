9782da6ddd54b01ed7ba5e2695fb13b6f60dcf50
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-01-05 13:57:13 +0100

    added visits, updated owner with pet relationship. closes #22.

--- pet-clinic-data/src/main/java/example/spring/project/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/example/spring/project/springpetclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package example.spring.project.springpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
