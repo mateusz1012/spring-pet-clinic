4d8394d83fed512c66665a6249ae94cc46e36c60
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-03-01 12:20:57 +0100

    added Specialties Map Impl. closes #27.

---
+++ pet-clinic-data/src/main/java/example/spring/project/springpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package example.spring.project.springpetclinic.services;
+
+import example.spring.project.springpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
