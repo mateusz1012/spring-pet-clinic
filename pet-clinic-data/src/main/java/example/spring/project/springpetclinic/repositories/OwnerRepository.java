be2d57ad20349ad5c8420e4c351e2b5b18bc0150
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-03-02 15:02:52 +0100

    added spring data jpa service for owners. closes #36.

--- pet-clinic-data/src/main/java/example/spring/project/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/example/spring/project/springpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import example.spring.project.springpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
