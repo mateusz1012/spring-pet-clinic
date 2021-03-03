8f345c041b49b13a2b3c350ed9d98f9853f9c51d
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-03-03 18:41:42 +0100

    implemented Map impl for Visit Service. closes #42.

---
+++ pet-clinic-data/src/main/java/example/spring/project/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package example.spring.project.springpetclinic.services;
+
+import example.spring.project.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
